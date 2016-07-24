5d9ee60c216151a47ef19359635fd8c9d3bf5465
Avi Zadok <Avizadok@gmail.com>
2016-07-24 23:05:49 +0300

    added

--- JsChallenges/app.js
+++ JsChallenges/app.js
@@ -1,1 +1,4 @@
 ﻿console.log('Hello world');
+
+
+
---
+++ JsChallenges/pointInCircle.js
@@ -0,0 +1,11 @@
+﻿
+var fs  = require("fs");
+fs.readFileSync(process.argv[2])
+    .toString()
+    .split('\n')
+    .forEach(function(line) {
+        if (line !== "") {
+            //do something here
+            //console.log(answer_line);
+        }
+    });
